e6a2bd5fd42182e69cf9365fcea126ca2ec61c1e
Tanutsun <Tanutsun@gmail.com>
2020-08-16 16:17:35 +0700

    add apm client

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var apm = require('elastic-apm-node').start({
   // Use if APM Server requires a token
   secretToken: '',
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://apm-server:8200'
+  serverUrl: 'http://apmserverlink:8200'
 })
 
 var express = require('express');
